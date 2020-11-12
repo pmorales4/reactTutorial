2027a7536bc88fa29615c65f893b1fb80a491696
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-11-12 15:19:39 -0500

    about to use hooks to refactor learning this tutorial

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
 
   // Search Github users
@@ -37,6 +38,16 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // gert users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}$client_secret=$
+    {process.env.REACT_APP_GITHBU_CLIENT_SECRET}`);
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -48,7 +59,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, loading, repos } = this.state;
     return (
       <Router>
         <div className='App'>
@@ -75,8 +86,15 @@ class App extends Component {
               <Route
                 exact
                 path='/user/:login'
-                render={props => (
-                  <User { ...props } getUser={this.getUser} user={user}  loading={loading}/>
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
+                  />
                 )}
               />
             </Switch>
--- src/components/users/user.js
+++ src/components/users/user.js
@@ -1,17 +1,21 @@
 import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export class user extends Component {
+class user extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   };
 
   render() {
@@ -31,7 +35,7 @@ export class user extends Component {
       hireable,
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -92,13 +96,13 @@ export class user extends Component {
             </ul>
           </div>
         </div>
-
         <div className='card text-center'>
-                <div className='badge badge-primary'>Followers: {followers}</div>
-                <div className='badge badge-success'>Followering: {following}</div>
-                <div className='badge badge-white'>Public Repos: {public_repos}</div>
-                <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Followering: {following}</div>
+          <div className='badge badge-white'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
