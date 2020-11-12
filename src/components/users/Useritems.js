78ca3cf7850797c3b5b89a09e5b948add43c3b85
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-11-11 20:44:48 -0500

    First commit of files from project

---
+++ src/components/users/Useritems.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Useritems = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+
+      <div>
+        <a href={html_url} className='a btn btn-dark btn-sm my-1'>
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+Useritems.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default Useritems;
